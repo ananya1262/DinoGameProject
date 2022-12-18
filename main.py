dcc6875ce336b7ba527f8613d86ed7e6f6996904
Ananya Garg <103451175+ananya1262@users.noreply.github.com>
2022-12-18 23:03:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,41 +22,41 @@ while True:
     key_count=0
     key_pressed=0
     hands,img=detector.findHands(frame)
-    cv2.rectangle(img,(0,480),(300,425),(50,50,255),-2)
-    cv2.rectangle(img,(640,480),(350,425),(50,50,255),-2)
+    cv2.rectangle(img,(0,480),(300,425),(128,0,0),-2)
+    cv2.rectangle(img,(640,480),(350,425),(128,0,0),-2)
     if hands:
         lmList=hands[0]
         fingerUp=detector.fingersUp(lmList)
-        print(fingerUp)
+        print(fingerUp)          
         if fingerUp==[0,0,0,0,0]:
-            cv2.putText(frame,'Finger Count: 0',(20,460),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),1,cv2.LINE_AA)
-            cv2.putText(frame,'Jumping',(420,460),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),1,cv2.LINE_AA)
-            PressKey(space_key_pressed)
-            space_pressed=True
-            current_key_pressed.add(space_key_pressed)
-            key_pressed=space_key_pressed
-            key_pressed=True
-            key_count=key_count+1
+            cv2.putText(frame,'Finger Count: 0',(20,460),cv2.FONT_HERSHEY_COMPLEX,1,(255,250,240),1,cv2.LINE_AA)
+            cv2.putText(frame,'Jump',(420,460),cv2.FONT_HERSHEY_COMPLEX,1,(255,250,240),1,cv2.LINE_AA)
+            # PressKey(space_key_pressed)
+            # space_pressed=True
+            # current_key_pressed.add(space_key_pressed)
+            # key_pressed=space_key_pressed
+            # key_pressed=True
+            # key_count=key_count+1
              
         if fingerUp==[0,1,0,0,0]:
-            cv2.putText(frame,'Finger Count: 1',(20,460),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),1,cv2.LINE_AA)
-            cv2.putText(frame,'Not Jumping',(390,460),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),1,cv2.LINE_AA)
+            cv2.putText(frame,'Finger Count: 1',(20,460),cv2.FONT_HERSHEY_COMPLEX,1,(255,250,240),1,cv2.LINE_AA)
+            # cv2.putText(frame,'Not Jumping',(390,460),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),1,cv2.LINE_AA)
        
         if fingerUp==[0,1,1,0,0]:
-            cv2.putText(frame,'Finger Count: 2',(20,460),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),1,cv2.LINE_AA)
-            cv2.putText(frame,'Not Jumping',(390,460),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),1,cv2.LINE_AA)
+            cv2.putText(frame,'Finger Count: 2',(20,460),cv2.FONT_HERSHEY_COMPLEX,1,(255,250,240),1,cv2.LINE_AA)
+            # cv2.putText(frame,'Not Jumping',(390,460),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),1,cv2.LINE_AA)
        
         if fingerUp==[0,1,1,1,0]:
-            cv2.putText(frame,'Finger Count: 3',(20,460),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),1,cv2.LINE_AA)
-            cv2.putText(frame,'Not Jumping',(390,460),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),1,cv2.LINE_AA)
+            cv2.putText(frame,'Finger Count: 3',(20,460),cv2.FONT_HERSHEY_COMPLEX,1,(255,250,240),1,cv2.LINE_AA)
+            # cv2.putText(frame,'Not Jumping',(390,460),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),1,cv2.LINE_AA)
        
         if fingerUp==[0,1,1,1,1]:
-            cv2.putText(frame,'Finger Count: 4',(20,460),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),1,cv2.LINE_AA)
-            cv2.putText(frame,'Not Jumping',(390,460),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),1,cv2.LINE_AA)
+            cv2.putText(frame,'Finger Count: 4',(20,460),cv2.FONT_HERSHEY_COMPLEX,1,(255,250,240),1,cv2.LINE_AA)
+            # cv2.putText(frame,'Not Jumping',(390,460),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),1,cv2.LINE_AA)
        
         if fingerUp==[1,1,1,1,1]:
-            cv2.putText(frame,'Finger Count: 5',(20,460),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),1,cv2.LINE_AA)
-            cv2.putText(frame,'Not Jumping',(390,460),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),1,cv2.LINE_AA)
+            cv2.putText(frame,'Finger Count: 5',(20,460),cv2.FONT_HERSHEY_COMPLEX,1,(255,250,240),1,cv2.LINE_AA)
+            # cv2.putText(frame,'Not Jumping',(390,460),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),1,cv2.LINE_AA)
         if not key_pressed and len(current_key_pressed)!=0:
             for key in current_key_pressed:
                 ReleaseKey(key)
